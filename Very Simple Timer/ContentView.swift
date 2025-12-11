2ea2392702318f5e73670c08f56a2b99e74f0f51
Thibaud Arguillere <thibaud.arguillere@hyland.com>
2025-12-11 17:36:36 +0100

    Initial Commit

---
+++ Very Simple Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Very Simple Timer
+//
+//  Created by Thibaud Arguillere on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Very Simple Timer/Very_Simple_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Very_Simple_TimerApp.swift
+//  Very Simple Timer
+//
+//  Created by Thibaud Arguillere on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Very_Simple_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
